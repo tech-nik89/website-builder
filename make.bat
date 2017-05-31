f187250c3ab8f2c2d9d5dc59f38b641815ec50b3
Nikolaus <tech-nik89@users.noreply.github.com>
2017-05-31 22:20:48 +0200

    Added make file

---
+++ make.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+set INNO_PATH="C:\Program Files (x86)\Inno Setup 5\Compil32.exe"
+
+rem UPDATE VERSION INFO
+echo Start Update Version Info
+SubWCRev %cd% %cd%\src\WebsiteBuilder.SharedSources\SharedAssemblyInfo.templ %cd%\src\WebsiteBuilder.SharedSources\SharedAssemblyInfo.cs
+echo End   Update Version Info
+
+rem BUILD SOLUTION
+echo Start Build Solution
+
+call %MSBUILD_PATH% src/WebsiteBuilder.sln /t:Clean /p:Configuration=Release
+call %MSBUILD_PATH% src/WebsiteBuilder.sln /t:Rebuild /p:Configuration=Release
+
+echo End   Build Solution
+
+rem BUILD SETUP
+echo Start Build Setup
+
+call %INNO_PATH% /cc "setup/setup.iss"
+
+echo End   Build Setp
