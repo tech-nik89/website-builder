29d4f974ec265f00520e3c45b565e943d4088704
Nikolaus <tech-nik89@users.noreply.github.com>
2017-06-14 16:27:43 +0200

    Re-applied changes for compiler console

--- src/Compiler/Options.cs
+++ src/Compiler/Options.cs
@@ -3,19 +3,27 @@ using System;
 using System.Text;
 
 namespace WebsiteBuilder.CompilerConsole {
-	class Options {
-		
-		[ValueOption(0)]
-		public String ProjectFile { get; set; }
+    class Options {
 
-		[HelpOption]
-		public String GetUsage() {
-			StringBuilder usage = new StringBuilder();
+        [ValueOption(0)]
+        public String ProjectFile { get; set; }
 
-			usage.AppendLine("Website Builder Compiler Console");
+        [Option('w', "wait")]
+        public bool Wait { get; set; }
 
-			return usage.ToString();
-		}
+        [HelpOption]
+        public String GetUsage() {
+            StringBuilder usage = new StringBuilder();
 
-	}
+            usage.AppendLine("Website Builder Compiler Console");
+            usage.AppendLine("--------------------------------");
+            usage.AppendLine("Syntax: WebsiteBuilder.Compiler.exe ProjectFile");
+            usage.AppendLine();
+            usage.AppendLine("     ProjectFile   The full path to the project file to compile.");
+            usage.AppendLine("  -w Wait          Wait for user input after the compile has completed.");
+
+            return usage.ToString();
+        }
+
+    }
 }
--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -1,36 +1,63 @@
 ﻿using CommandLine;
 using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 using WebsiteBuilder.Core;
 using WebsiteBuilder.Core.Compiling;
+using WebsiteBuilder.Core.Plugins;
 
 namespace WebsiteBuilder.CompilerConsole {
-	class Program {
-		static void Main(string[] args) {
+    class Program {
+        static void Main(string[] args) {
 
-			Options options = new Options();
+            Options options = new Options();
 
-			if (!Parser.Default.ParseArguments(args, options)) {
-				Console.WriteLine(options.GetUsage());
-				return;
-			}
+            if (!Parser.Default.ParseArguments(args, options)) {
+                Console.WriteLine(options.GetUsage());
+                return;
+            }
 
-			try {
-				Project project = Project.Load(options.ProjectFile);
-				Compiler compiler = new Compiler(project);
+            if (String.IsNullOrWhiteSpace(options.ProjectFile)
+                || !File.Exists(options.ProjectFile)) {
 
-				compiler.Completed += (sender, e) => {
+                Console.WriteLine(options.GetUsage());
+                return;
+            }
 
-				};
+            try {
+                PluginManager.Init();
 
-				compiler.ProgressChanged += (sender, e) => {
+                Project project = Project.Load(options.ProjectFile);
+                Compiler compiler = new Compiler(project);
+                AutoResetEvent wait = new AutoResetEvent(false);
 
-				};
+                compiler.Completed += (sender, e) => {
+                    wait.Set();
+                };
 
-				compiler.StartAsync();
-			}
-			catch (Exception e) {
-				Console.WriteLine(e.Message);
-			}
-		}
-	}
+                compiler.ProgressChanged += (sender, e) => {
+                    StringBuilder message = new StringBuilder();
+
+                    message.Append(e.Percentage.ToString().PadRight(3));
+                    message.Append("% ");
+                    message.Append(e.Message);
+
+                    Console.WriteLine(message.ToString());
+                };
+
+                compiler.StartAsync();
+                wait.WaitOne();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            if (options.Wait) {
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadLine();
+            }
+        }
+    }
 }
